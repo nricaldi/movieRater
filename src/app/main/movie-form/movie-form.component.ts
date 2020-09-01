265e4d1e967b979f0d75b60fe66b52f56dd1f7ea
Nicolas Ricaldi <nicolasricaldi@Nicolass-MBP.fios-router.home>
2020-09-01 19:15:14 -0400

    implemented front end validation for movie form

--- src/app/main/movie-form/movie-form.component.ts
+++ src/app/main/movie-form/movie-form.component.ts
@@ -10,8 +10,11 @@ import { ApiService } from '../../api.service';
 })
 export class MovieFormComponent implements OnInit {
 
-  movieForm : FormGroup;
-  id = null;
+  public movieForm : FormGroup;
+  public id = null;
+  public formError : string;
+  public titleError : string;
+  public descriptionError : string;
 
   @Output() movieCreated = new EventEmitter<Movie>();
   @Output() movieUpdated = new EventEmitter<Movie>();
@@ -32,24 +35,58 @@ export class MovieFormComponent implements OnInit {
   }
 
   saveForm() {
-
-    // updates the existing movie if the id is given
-    if (this.id) {
-      let observable = this.apiService.updateMovie(this.id, this.movieForm.value.title, this.movieForm.value.description);
-      observable.subscribe(
-        (updatedMovie: Movie) => this.movieUpdated.emit(updatedMovie),
-        errors => console.log(errors)
-      );
+    if(this.checkForm()) {
+      // updates the existing movie if the id is given
+      if (this.id) {
+        let observable = this.apiService.updateMovie(this.id, this.movieForm.value.title, this.movieForm.value.description);
+        observable.subscribe(
+          (updatedMovie: Movie) => this.movieUpdated.emit(updatedMovie),
+          errors => console.log(errors)
+        );
+      }
+  
+      // creates a new movie with form values
+      else {
+        let observable = this.apiService.createMovie(this.movieForm.value.title, this.movieForm.value.description);
+        observable.subscribe(
+          (newMovie: Movie) => this.movieCreated.emit(newMovie),
+          error => console.log(error)
+        )}
     }
-
-    // creates a new movie with form values
-    else {
-      let observable = this.apiService.createMovie(this.movieForm.value.title, this.movieForm.value.description);
-      observable.subscribe(
-        (newMovie: Movie) => this.movieCreated.emit(newMovie),
-        error => console.log(error)
-      )}
 
   }
 
-}
+   // Form Validation
+   checkForm() {
+    let isValidated = true;
+
+    // check if form is empty
+    if (this.movieForm.value.title.length == 0 &&
+        this.movieForm.value.description.length == 0) {
+          this.formError = 'Form cannot be empty';
+          console.log(this.formError);
+          isValidated = false;
+    } else {
+      // check if title is 1+ characters in length
+      if (this.movieForm.value.title.length < 1 ) {
+        this.titleError = 'title must be at least 1 character';
+        isValidated = false;
+      } else {
+        // need to set as empty string to erase validation next time the form is submitted
+          this.titleError = '';
+        }
+  
+      // check if description is 10+ characters in length
+      if (this.movieForm.value.description.length < 10 ) {
+        this.descriptionError = 'password must be at least 8 characters';
+        isValidated = false;
+      } else {
+          this.descriptionError = '';
+        }
+    }
+
+
+    return isValidated;
+  }
+
+} 
